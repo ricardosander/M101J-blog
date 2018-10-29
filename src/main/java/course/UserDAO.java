c76306989f0cb86c0c0695a3362117f3c1409f82
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-10-29 03:01:48 -0300

    Solving Homework 2.4 : inserting and retrieving a user from MongoDB

--- src/main/java/course/UserDAO.java
+++ src/main/java/course/UserDAO.java
@@ -44,19 +44,14 @@ public class UserDAO {
 
         String passwordHash = makePasswordHash(password, Integer.toString(random.nextInt()));
 
-        // XXX WORK HERE
-        // create an object suitable for insertion into the user collection
-        // be sure to add username and hashed password to the document. problem instructions
-        // will tell you the schema that the documents must follow.
+        Document document = new Document("_id", username).append("password", passwordHash);
 
         if (email != null && !email.equals("")) {
-            // XXX WORK HERE
-            // if there is an email address specified, add it to the document too.
+            document = document.append("email", email);
         }
 
         try {
-            // XXX WORK HERE
-            // insert the document into the user collection here
+            usersCollection.insertOne(document);
             return true;
         } catch (MongoWriteException e) {
             if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
@@ -68,10 +63,8 @@ public class UserDAO {
     }
 
     public Document validateLogin(String username, String password) {
-        Document user = null;
 
-        // XXX look in the user collection for a user that has this username
-        // assign the result to the user variable.
+        Document user = usersCollection.find(new Document("_id", username)).first();
 
         if (user == null) {
             System.out.println("User not in database");
